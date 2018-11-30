4c748e569ef67c34ecfd2c324cd98f3ce29a69ff
Tylor Jilk <tjilk2663@gmail.com>
2018-11-29 18:45:51 -0800

    updating

--- dev_board_driver/dev_board_driver.ino
+++ dev_board_driver/dev_board_driver.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
 #include <TMC26XStepper.h>
 
-//we have a stepper motor with 200 steps per rotation, CS pin 2, dir pin 6, step pin 7 and a current of 300mA
+//we have a stepper motor with 200 steps per rotation, CS pin 10, dir pin 6, step pin 7 and a current of 300mA
 const int steps_per_rev = 200;
-const int cs_pin = 4;
+const int cs_pin = 10;
 const int dir_pin = 6;
-const int step_pin = 5;
+const int step_pin = 7;
 TMC26XStepper tmc26XStepper = TMC26XStepper(steps_per_rev, cs_pin, dir_pin, step_pin, 700);
 
 int curr_step;
@@ -13,13 +13,16 @@ int speed =  0;
 int speedDirection = 100;
 int maxSpeed = 1000;
 
-const int right_pin = 1;
-const int left_pin = 2;
-const int run_pin = 3;
-const int stop_pin = 7;
-const int led_pin = 13;
+/*
+  const int right_pin = 1;
+  const int left_pin = 2;
+  const int run_pin = 3;
+  const int stop_pin = 7;
+  const int led_pin = 13;
+*/
 
 const int serial_speed = 9600;
+int counter = 0;
 
 // 0 = stopped; 1 = run; 2 = right; 3 = left
 int motor_state = 0;
@@ -33,9 +36,10 @@ void setup() {
 
 void loop() {
   driveMotor();
+  counter += 1;
 }
 
-void startSerial(){
+void startSerial() {
   // Start the serial communication
   Serial.begin(serial_speed);
   // Print the header
@@ -44,7 +48,7 @@ void startSerial(){
   Serial.println("==============================");
 }
 
-void startMotor(){
+void startMotor() {
   Serial.println("Configuring stepper driver");
   //char constant_off_time, char blank_time, char hysteresis_start, char hysteresis_end, char hysteresis_decrement
   tmc26XStepper.setSpreadCycleChopper(2, 24, 8, 6, 0);
@@ -59,48 +63,97 @@ void startMotor(){
   Serial.println("started");
 }
 
-void initializePins(){
+void initializePins() {
   pinMode(cs_pin, OUTPUT);
   pinMode(dir_pin, OUTPUT);
   pinMode(step_pin, OUTPUT);
-  pinMode(led_pin, OUTPUT);
-  pinMode(right_pin, INPUT);
-  pinMode(left_pin, INPUT);
-  pinMode(run_pin, INPUT);
-  pinMode(stop_pin, INPUT);
+
+  /*
+    pinMode(led_pin, OUTPUT);
+    pinMode(right_pin, INPUT);
+    pinMode(left_pin, INPUT);
+    pinMode(run_pin, INPUT);
+    pinMode(stop_pin, INPUT);
+  */
 }
 
-void initializeInterrupts(){
-  attachInterrupt(digitalPinToInterrupt(right_pin), RightMotor, RISING);
-  attachInterrupt(digitalPinToInterrupt(left_pin), LeftMotor, RISING);
-  attachInterrupt(digitalPinToInterrupt(run_pin), RunMotor, RISING);
-  attachInterrupt(digitalPinToInterrupt(stop_pin), StopMotor, RISING);
+void initializeInterrupts() {
+  /*
+    attachInterrupt(digitalPinToInterrupt(right_pin), RightMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(left_pin), LeftMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(run_pin), RunMotor, RISING);
+    attachInterrupt(digitalPinToInterrupt(stop_pin), StopMotor, RISING);
+  */
 }
 
-void RightMotor(){
+void RightMotor() {
   motor_state = 2;
 }
 
-void LeftMotor(){
+void LeftMotor() {
   motor_state = 3;
 }
 
-void RunMotor(){
+void RunMotor() {
   motor_state = 1;
 }
 
-void StopMotor(){
+void StopMotor() {
   motor_state = 0;
 }
 
-void driveMotor(){
+void driveMotor() {
+  if (!tmc26XStepper.isMoving()) {
+    speed += speedDirection;
+    if (speed > maxSpeed) {
+      speed = maxSpeed;
+      speedDirection = -speedDirection;
+    } else if (speed < 0) {
+      speedDirection = -speedDirection;
+      speed = speedDirection;
+    }
+    //setting the speed
+    Serial.print("setting speed to ");
+    Serial.println(speed);
+    tmc26XStepper.setSpeed(speed);
+    //we want some kind of constant running time - so the length is just a product of speed
+    Serial.print("Going ");
+    Serial.print(10 * speed);
+    Serial.println(" steps");
+    tmc26XStepper.step(10 * speed);
+  } else {
+    //we put out the status every 100 steps
+    if (tmc26XStepper.getStepsLeft() % 100 == 0) {
+      // Serial.print("Stall Guard: ");
+      tmc26XStepper.getCurrentStallGuardReading();
+    }
+  }
+  tmc26XStepper.move();
+  
+  if (counter % 50000 == 0) {
+    bool loada = tmc26XStepper.isOpenLoadA();
+    if (loada == true) {
+      Serial.println("Load A: connected");
+    } else if (loada == false) {
+      Serial.println("Load A: not connected");
+    }
+    bool loadb = tmc26XStepper.isOpenLoadB();
+    if (loadb == true) {
+      Serial.println("Load B: connected");
+    } else if (loadb == false) {
+      Serial.println("Load B: not connected");
+    }
+  }
+}
+/*
+  void driveMotor(){
   switch (motor_state){
     case 0: // Stopped
       digitalWrite(led_pin, LOW);
       tmc26XStepper.stop();
       break;
     case 1: // Run back and forth
-      
+
       break;
     case 2: // Right Turning
       digitalWrite(led_pin, HIGH);
@@ -132,7 +185,8 @@ void driveMotor(){
       tmc26XStepper.move();
       break;
     case 3: // Left Turning
-      
+
       break;
   }
-}
+  }
+*/
