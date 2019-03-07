56cbfc8a45c5585b7d213f712c33ef5f82eaff55
Tylor Jilk <tjilk2663@gmail.com>
2019-03-06 18:54:21 -0800

    updating

---
+++ 2018-19/SAMD21_StepperTest_01/SAMD21_StepperTest_01.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <TMC26XStepper.h>
+
+// These values are for h.boson_1.0
+// 200 steps per rotation, CS pin 7, dir pin 36, step pin 31 and a current of 300mA
+
+TMC26XStepper stepboi = TMC26XStepper(200,7,36,31,700);
+
+int step_speed =  200;
+
+void setup() {
+  Serial.begin(9600);
+  //set this according to you stepper
+  Serial.println("Configuring stepper driver");
+  
+  //char constant_off_time, char blank_time, char hysteresis_start, char hysteresis_end, char hysteresis_decrement
+  stepboi.setSpreadCycleChopper(2,24,8,6,0);
+  stepboi.setRandomOffTime(0);
+  
+  stepboi.setMicrosteps(32);
+  stepboi.setStallGuardThreshold(4,0);
+  Serial.println("config finished, starting");
+  stepboi.start();
+  Serial.println("started");
+}
+
+void loop() {
+  // First step one direction and print out stall guard.
+  stepboi.setSpeed(step_speed);
+  stepboi.step(step_speed);
+  stepboi.move();
+  if (stepboi.getStepsLeft()%100==0) {
+    Serial.print("Stall Guard: ");
+    Serial.println(stepboi.getCurrentStallGuardReading());
+  }
+  delay(5000); // delay 5 seconds
+
+  // Next step the opposite direction
+  stepboi.step(-step_speed);
+  stepboi.move();
+  if (stepboi.getStepsLeft()%100==0) {
+    Serial.print("Stall Guard: ");
+    Serial.println(stepboi.getCurrentStallGuardReading());
+  }
+  delay(5000); // delay 5 seconds
+}
