1e5cea24fa3b2d0f32a570d3ad76fa1439f6e28b
Tylor Jilk <tjilk2663@gmail.com>
2019-04-25 10:54:54 -0700

    changed to serialusb and edited spi transfer function

--- 2018-19/Testing_Code/SAMD21_StepperTest_02/SAMD21_StepperTest_02.ino
+++ 2018-19/Testing_Code/SAMD21_StepperTest_02/SAMD21_StepperTest_02.ino
@@ -16,18 +16,24 @@ const int md_en = 37;         // PA22 on Arduino M0
 const int md_sgtst = 35;      // PB22 on Arduino M0
 
 // Motor Driver configuration bytes
-byte chopconf = 10010000000110110100; // normal startup
-byte sgcsconf = 11010000000000011111; // full current for both SPI and step/dir operation
-byte drvconf = 11101111000000010000; // step dir interface enabled
-byte drvctrl = 00000000000000001000; // fullstep, pulse interpolation disabled, only rising STEP edge active
-byte smarten = 10101000001000000010; // coolStep control register - normal setup
+// byte chopconf = 10010000000110110100; // normal startup
+// byte sgcsconf = 11010000000000011111; // full current for both SPI and step/dir operation
+// byte drvconf = 11101111000000010000; // step dir interface enabled
+// byte drvctrl = 00000000000000001000; // fullstep, pulse interpolation disabled, only rising STEP edge active
+// byte smarten = 10101000001000000010; // coolStep control register - normal setup
+
+unsigned long chopconf = 0x901B4ul;
+unsigned long sgcsconf = 0xD001F;
+unsigned long drvconf = 0xEF010;
+unsigned long drvctrl = 0x8;
+unsigned long smarten = 0xA8202;
 
 const int enable = 16;
 int step_speed =  200;
 
 void setup() {
   SPI.begin();
-  Serial.begin(9600);
+  SerialUSB.begin(9600);
   
   pinMode(md_en, OUTPUT);
   pinMode(md_chipSelect, OUTPUT);  
@@ -39,7 +45,7 @@ void setup() {
   digitalWrite(md_dir, LOW); // choose a direction to begin with
   digitalWrite(md_clk, LOW); // writing low makes the chip use its on-board oscillator
 
-  Serial.println("Configuring chopper control register");
+  SerialUSB.println("Configuring chopper control register");
   
   writeMotor(chopconf);
   writeMotor(sgcsconf);
@@ -48,10 +54,17 @@ void setup() {
   writeMotor(smarten);
 }
 
-void writeMotor(byte command){
+void writeMotor(unsigned long command){
   digitalWrite(md_chipSelect, LOW);
-  Serial.println(SPI.transfer(command));
+  unsigned long response;
+  response = SPI.transfer((command >> 16) & 0xff);
+  response <<= 8;
+  response |= SPI.transfer((command >>  8) & 0xff);
+  response <<= 8;
+  response |= SPI.transfer((command) & 0xff);
+  response >>= 4;
   digitalWrite(md_chipSelect, HIGH);
+  SerialUSB.println(response, HEX);
 }
 
 void loop() {
@@ -59,7 +72,7 @@ void loop() {
   digitalWrite(md_dir, !digitalRead(md_dir));
   delay(10);
   digitalWrite(md_step, HIGH);
-  Serial.println("Stepping...");
+  SerialUSB.println("Stepping...");
   delay(1000); // min delay is 10 ns, so we should be good
   digitalWrite(md_step, LOW);
   delay(1000);
